e149e97a3d991b7cbac939acbd548207ca67bb0c
Jared Hanson <jaredhanson@gmail.com>
2022-01-11 19:47:53 -0800

    Add tests.

--- test/page.test.js
+++ test/page.test.js
@@ -22,4 +22,22 @@ describe('Page', function() {
   
   }); // #render
   
+  describe('#compile', function() {
+    
+    it('should compile', function(done) {
+      chai.kerouac.page(function(page, next) {
+        var rv = page.compile('# Hello', 'md', 'index');
+        expect(rv).to.be.undefined;
+      })
+      .finish(function() {
+        expect(this._text).to.equal('# Hello');
+        expect(this._type).to.equal('md');
+        expect(this._layout).to.equal('index');
+        done();
+      })
+      .generate();
+    }); // should render
+    
+  });
+  
 });
